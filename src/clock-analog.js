fb0c0131fecc6d739600c4879c1cf505c94a071f
lym <lym91@hatiolab.com>
2017-11-24 07:13:35 +0900

    수정

--- src/clock-analog.js
+++ src/clock-analog.js
@@ -1,6 +1,54 @@
 /*
  * Copyright © HatioLab Inc. All rights reserved.
  */
+
+const NATURE = {
+  mutable: false,
+  resizable: true,
+  rotatable: true,
+  properties : [{
+    type: 'number',
+    label: 'hourwidth',
+    name: 'hourWidth',
+    property: 'hourWidth'
+  },{
+    type: 'number',
+    label: 'minute-width',
+    name: 'minuteWidth',
+    property: 'minuteWidth'
+  },{
+    type: 'number',
+    label: 'second-width',
+    name: 'secondWidth',
+    property: 'secondWidth'
+  },{
+    type: 'checkbox',
+    label: 'show-second',
+    name: 'showSecond',
+    property: 'showSecond'
+  },{
+    type: 'checkbox',
+    label: 'show-number',
+    name: 'showNumber',
+    property: 'showNumber'
+  },{
+    type: 'checkbox',
+    label: 'fill-interval',
+    name: 'fillInterval',
+    property: 'fillInterval'
+  },{
+    type: 'color',
+    label: 'fill-interval-color',
+    name: 'fillIntervalColor',
+    property: 'fillIntervalColor'
+  },{
+    type: 'color',
+    label: 'inner-circle-color',
+    name: 'innerCircleColor',
+    property: 'innerCircleColor'
+  }]
+}
+
 function drawHand(ctx, pos, length, rx) {
     ctx.beginPath();
     ctx.lineWidth = rx;
@@ -21,42 +69,52 @@ export default class ClockAnalog extends scene.Ellipse {
       fillStyle,
       strokeStyle,
       lineWidth,
+      hourWidth,
+      minuteWidth,
+      secondWidth,
+      showSecond = true,
+      showNumber = true,
+      fillInterval = false,
+      fillIntervalColor,
+      innerCircleColor = '#FF0000'
     } = this.model;
+
     // 시계 원 그리기.
     ctx.beginPath();
     ctx.ellipse(cx, cy, Math.abs(rx), Math.abs(ry), 0, 0, 2 * Math.PI)
     this.drawFill(ctx)
 
     ctx.strokeStyle = strokeStyle
-    ctx.lineWidth = rx * 0.1;
+    ctx.lineWidth = lineWidth || rx * 0.1;
+    ctx.fillStyle = fillStyle
+
     ctx.stroke();
     ctx.beginPath();
-    ctx.ellipse(cx, cy, Math.abs(rx * 0.1), Math.abs(ry * 0.1), 0, 0, 2 * Math.PI)
-    ctx.fillStyle = strokeStyle
-    ctx.fill();
-
+    
     ctx.translate(cx, cy);
     ctx.scale(1, ry / rx)
 
 
     // 시계 숫자 그리기
-    var ang;
-    var num;
-    ctx.font = rx * 0.15 + "px arial";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
+    if(showNumber){
+      var ang;
+      var num;
+      ctx.font = rx * 0.15 + "px arial";
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "center";
 
-    for(num = 1; num < 13; num++){
-      ang = num * Math.PI / 6;
-      ctx.rotate(ang);
-      ctx.translate(0, - rx * 0.85);
-      ctx.rotate(-ang);
-      ctx.fillText(num.toString(), 0, 0);
-      ctx.rotate(ang);
-      ctx.translate(0, rx * 0.85);
-      ctx.rotate(-ang);
+      for(num = 1; num < 13; num++){
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, - rx * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, rx * 0.85);
+        ctx.rotate(-ang);
+      }
     }
-
+    
     // 시계 침 그리기
     var now = new Date();
     var hour = now.getHours();
@@ -68,16 +126,25 @@ export default class ClockAnalog extends scene.Ellipse {
     (minute * Math.PI / (6 * 60)) +
     (second * Math.PI / (360 * 60));
 
-    drawHand(ctx, hour, rx * 0.5, rx * 0.07);
+    drawHand(ctx, hour, rx * 0.5, hourWidth || rx * 0.07);
+
     //minute
     minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-    drawHand(ctx, minute, rx * 0.8, rx * 0.07);
+    drawHand(ctx, minute, rx * 0.8, minuteWidth || rx * 0.07);
+
     // second
-    second = (second * Math.PI / 30);
-    drawHand(ctx, second, rx * 0.9, rx * 0.02);
+    if(showSecond){
+      second = (second * Math.PI / 30);
+      drawHand(ctx, second, rx * 0.9, secondWidth || rx * 0.02);
+    }
 
     ctx.scale(1, 1)
     ctx.translate(-cx, -cy);
+
+    ctx.beginPath();
+    ctx.ellipse(cx, cy, Math.abs(rx * 0.1), Math.abs(ry * 0.1), 0, 0, 2 * Math.PI)
+    ctx.fillStyle = innerCircleColor
+    ctx.fill();
 
 
     var timeOut
