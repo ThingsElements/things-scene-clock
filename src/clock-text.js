53d87916d73d8f0f26d553cd84529c9a16489daf
EunHyeYun <lillyeh@hatiolab.com>
2019-05-15 16:05:10 +0900

    요일 표시 언어 선택 시 deprecated인 부분 수정
    moment.lang을 moment.locale로 변경

--- src/clock-text.js
+++ src/clock-text.js
@@ -2,86 +2,88 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
- import { Component, Text } from '@hatiolab/things-scene'
- import moment from 'moment';
+import { Component, Text } from "@hatiolab/things-scene";
+import moment from "moment";
 
- const NATURE = {
+const NATURE = {
   mutable: false,
   resizable: true,
   rotatable: true,
-  properties : [{
-    type: 'string',
-    label: 'time-format',
-    name: 'timeFormat'
-  }, {
-    type: 'checkbox',
-    label: 'is-local-time',
-    name: 'localTime'
-  }, {
-    type: 'number',
-    label: 'utc',
-    name: 'utc'
-  }, {
-    type: 'select',
-    label : 'week-language',
-    name : 'weekLanguage',
-    property: {
-      options: [
-        {
-          display: "English",
-          value: "en"
-        },
-        {
-          display: "Korean",
-          value: "ko"
-        },
-        {
-          display: "Chinese",
-          value: "zh_cn"
-        },
-        {
-          display: "Japanese",
-          value: "ja"
-        }
-      ]
+  properties: [
+    {
+      type: "string",
+      label: "time-format",
+      name: "timeFormat"
+    },
+    {
+      type: "checkbox",
+      label: "is-local-time",
+      name: "localTime"
+    },
+    {
+      type: "number",
+      label: "utc",
+      name: "utc"
+    },
+    {
+      type: "select",
+      label: "week-language",
+      name: "weekLanguage",
+      property: {
+        options: [
+          {
+            display: "English",
+            value: "en"
+          },
+          {
+            display: "Korean",
+            value: "ko"
+          },
+          {
+            display: "Chinese",
+            value: "zh_cn"
+          },
+          {
+            display: "Japanese",
+            value: "ja"
+          }
+        ]
+      }
     }
-  }]
-}
+  ]
+};
 
 export default class ClockText extends Text {
-
   get nature() {
-    return NATURE
+    return NATURE;
   }
 
   _draw(ctx) {
-    setTimeout(this._timer.bind(this), 1000)
+    setTimeout(this._timer.bind(this), 1000);
   }
 
   _timer() {
     this.set({
       text: this._getTimeStamp()
-    })
+    });
   }
 
   _getTimeStamp() {
-    
     var d = moment();
 
-    var utc = this.get('utc')
-    var formatStr = this.get('timeFormat') || 'YYYY-MM-DD HH:mm:ss'
-    var week_lang = this.get('weekLanguage')
-    if(!this.get('weekLanguage')) {
-      week_lang = 'en'
+    var utc = this.get("utc");
+    var formatStr = this.get("timeFormat") || "YYYY-MM-DD HH:mm:ss";
+    var week_lang = this.get("weekLanguage");
+    if (!this.get("weekLanguage")) {
+      week_lang = "en";
     }
-    if(this.get('localTime')) {
+    if (this.get("localTime")) {
       d.local();
     } else {
       d.utc().utcOffset(utc);
     }
-    return d.lang(week_lang).format(formatStr)
+    return d.locale(week_lang).format(formatStr);
   }
-
 }
 
-Component.register('clock-text', ClockText)
+Component.register("clock-text", ClockText);
